a16d4c71f241d1d2f9075e9adcb29ab6a25f02c2
Joanderson <joanderson.rikudo@yahoo.com>
2020-06-09 18:01:47 -0300

    adapter getting recipe list from a func + MVVM pattern setted + retrieving data from database

--- app/src/main/java/joandersongoncalves/example/veganocook/presentation/CategoryActivity.kt
+++ app/src/main/java/joandersongoncalves/example/veganocook/presentation/CategoryActivity.kt
@@ -3,11 +3,14 @@ package joandersongoncalves.example.veganocook.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import joandersongoncalves.example.veganocook.R
-import joandersongoncalves.example.veganocook.data.model.YouTubeVideo
+import joandersongoncalves.example.veganocook.data.model.Recipe
 import joandersongoncalves.example.veganocook.presentation.adapter.RecipeAdapter
+import joandersongoncalves.example.veganocook.presentation.viewmodel.CategoryViewModel
 import kotlinx.android.synthetic.main.activity_category.*
 import kotlinx.android.synthetic.main.app_toolbar.*
 
@@ -17,68 +20,51 @@ class CategoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
 
-
+        // setting RecyclerView
+        val recipeAdapter = RecipeAdapter { recipe ->
+            val intent = RecipeDetailsActivity.getStartIntent(this@CategoryActivity, recipe)
+            this@CategoryActivity.startActivity(intent)
+        }
         with(rvRecipesByCategory) {
             layoutManager = GridLayoutManager(this@CategoryActivity, 2)
             setHasFixedSize(true)
-            adapter = RecipeAdapter(videoExemples()) { youtubeVideo ->
-                val intent =
-                    RecipeDetailsActivity.getStartIntent(this@CategoryActivity, youtubeVideo)
-                this@CategoryActivity.startActivity(intent)
-            }
+            adapter = recipeAdapter
         }
+
+        // setting viewModel
+        val viewModel: CategoryViewModel by viewModels()
+
+        intent.getStringExtra(EXTRA_CATEGORY_TITLE)?.let {
+            val category = when (it) {
+                getString(R.string.breakfest) -> Recipe.BREAKFAST
+                getString(R.string.lunch) -> Recipe.LUNCH
+                getString(R.string.dinner) -> Recipe.DINNER
+                getString(R.string.snack) -> Recipe.SNACK
+                else -> ""
+            }
+            viewModel.getCategoryWithRecipes(category)
+
+        }
+
+        viewModel.recipesByCategory.observe(this, Observer {
+            it?.let {
+                recipeAdapter.setRecipes(it)
+            }
+        })
 
         //setting the right toolbar for this activity
         viewFlipperAppToolbar.displayedChild = 1
         appToolbarOther.title = intent.getStringExtra(EXTRA_CATEGORY_TITLE)
-
         AppToolbarSetup.setBackButton(appToolbarOther, this)
     }
 
-    /*
-    companion object {
-        private const val EXTRA_VIDEO = "EXTRA_VIDEO"
-
-        fun getStartIntent(context: Context, video: YouTubeVideo): Intent {
-            return Intent(context,RecipeDetailsActivity::class.java).apply {
-                putExtra(EXTRA_VIDEO, video)
-            }
-        }
-    }
-    */
     companion object {
         private const val EXTRA_CATEGORY_TITLE = "EXTRA_CATEGORY_TITLE"
 
-        fun getStartActivity(conext: Context, title: String): Intent {
-            return Intent(conext, CategoryActivity::class.java).apply {
+        fun getStartActivity(context: Context, title: String): Intent {
+            return Intent(context, CategoryActivity::class.java).apply {
                 putExtra(EXTRA_CATEGORY_TITLE, title)
             }
         }
     }
-
-
-    private fun videoExemples(): List<YouTubeVideo> {
-        return listOf(
-            YouTubeVideo("Nhoque delicioso", "Este é um nhoque divino...", null, null),
-            YouTubeVideo(
-                "Lasanha de beringela",
-                "Este é uma lasanha de comer rezando...",
-                null,
-                null
-            ),
-            YouTubeVideo(
-                "Farofa proteica",
-                "Esta farofa vai te fazer chorar de alegria...",
-                null,
-                null
-            ),
-            YouTubeVideo(
-                "Smoothie verde",
-                "Este smoothie vai te fazer muito mais saudável...",
-                null,
-                null
-            )
-        )
-    }
-
 }
