9e5019f45cb80468b7d907dad754d941fb4e660e
Joanderson <joanderson.rikudo@yahoo.com>
2020-06-19 17:59:10 -0300

    search working + viewModel created

--- app/src/main/java/joandersongoncalves/example/veganocook/presentation/SearchActivity.kt
+++ app/src/main/java/joandersongoncalves/example/veganocook/presentation/SearchActivity.kt
@@ -2,40 +2,68 @@ package joandersongoncalves.example.veganocook.presentation
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
-import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import joandersongoncalves.example.veganocook.R
+import joandersongoncalves.example.veganocook.presentation.adapter.SearchResultAdapter
+import joandersongoncalves.example.veganocook.presentation.viewmodel.SearchViewModel
 import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.app_toolbar.*
 
 class SearchActivity : AppCompatActivity() {
     private var searchView: SearchView? = null
+    private val viewModel: SearchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         //setting RecyclerView
-        with(rvSearchResults) {
-            layoutManager = androidx.recyclerview.widget.GridLayoutManager(this@SearchActivity, 2)
-            setHasFixedSize(true)
-            adapter =
-                joandersongoncalves.example.veganocook.presentation.adapter.RecipeAdapter { recipe ->
-                    val intent = RecipeDetailsActivity.getStartIntent(this@SearchActivity, recipe)
-                this@SearchActivity.startActivity(intent)
-
-            }
+        val searchResultAdapter = SearchResultAdapter { recipe ->
+            val intent = RecipeDetailsActivity.getStartIntent(this@SearchActivity, recipe)
+            val reqCode = 1
+            this@SearchActivity.startActivityForResult(intent, reqCode)
         }
+        with(rvSearchResults) {
+            layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this@SearchActivity)
+            setHasFixedSize(true)
+            adapter = searchResultAdapter
+        }
+
+        //setting viewModel
+        viewModel.recipesResult.observe(this, Observer {
+            searchResultAdapter.updateRecipes(it)
+            if (viewFlipperSearch.displayedChild > 0 && it.isEmpty()) {
+                //no results found
+                viewFlipperSearch.displayedChild = 3
+            }
+            if (viewFlipperSearch.displayedChild == 1) {
+                viewFlipperSearch.displayedChild = 2
+            }
+        })
+
+        //setting viewFlipper
+        viewFlipperSearch.displayedChild = 0
 
         //setting SearchActivity toolbar
         viewFlipperAppToolbar.displayedChild = 2
         setSupportActionBar(appToolbarSearch)
         appToolbarSearch.title = ""
         AppToolbarSetup.setBackButton(appToolbarSearch, this)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RecipeDetailsActivity.RETURN_UPDATED_RECIPE) {
+            viewModel.query.value?.let { viewModel.queryByName(it) }
+        }
 
     }
 
@@ -52,8 +80,11 @@ class SearchActivity : AppCompatActivity() {
         searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
             override fun onQueryTextSubmit(query: String?): Boolean {
-                println("Query: $query")
-                Toast.makeText(this@SearchActivity, "Query: $query", Toast.LENGTH_LONG).show()
+                query?.let {
+                    viewModel.query.value = it
+                    viewModel.queryByName(it)
+                    viewFlipperSearch.displayedChild = 1
+                }
 
                 // Escondendo o teclado
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
