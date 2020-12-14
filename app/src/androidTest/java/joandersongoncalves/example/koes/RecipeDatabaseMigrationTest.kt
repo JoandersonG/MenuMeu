49d08eb7cb9878db9ef66dd1b846d7db9fde57cb
Joanderson <joandersongsantos@yahoo.com>
2020-12-13 22:26:34 -0300

    test: add test to database migration from 2 to 3

--- app/src/androidTest/java/joandersongoncalves/example/koes/RecipeDatabaseMigrationTest.kt
+++ app/src/androidTest/java/joandersongoncalves/example/koes/RecipeDatabaseMigrationTest.kt
@@ -6,6 +6,7 @@ import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import joandersongoncalves.example.koes.data.database.MIGRATION_1_2
+import joandersongoncalves.example.koes.data.database.MIGRATION_2_3
 import joandersongoncalves.example.koes.data.database.RecipeDatabase
 import org.junit.Rule
 import org.junit.Test
@@ -44,6 +45,28 @@ class RecipeDatabaseMigrationTest {
             2,
             true,
             MIGRATION_1_2
+        )
+    }
+
+    @Test
+    fun migration2to3() {
+        database = migrationTestHelper.createDatabase(TEST_DB, 2)
+            .apply {
+                execSQL(
+                    """
+                    INSERT INTO recipes 
+                    (name,description,ingredients,is_favorite, video_title,video_description,video_url,default_thumbnail_url, medium_thumbnail_url)
+                    VALUES('Bacalhonese vegana','Uma comida gostosa','uma batata, duas cebolas',1,'bacalhonese','descrição do vídeo bacalhonese','url bacalhonese','url thumb def','med thumb url')
+                """.trimIndent()
+                )
+                close()
+            }
+
+        database = migrationTestHelper.runMigrationsAndValidate(
+            TEST_DB,
+            3,
+            true,
+            MIGRATION_2_3
         )
     }
 }
